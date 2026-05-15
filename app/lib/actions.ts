b8398a3c37a6336672c2e87a8467fbcf22e7bbec
mrbryant21 <mrbryant250621@gmail.com>
2026-05-15 23:00:58 +0000

    working on error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,38 +18,52 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+    try{
+        const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
-    });
+        });
 
-    const amountInCents = Math.round(amount * 100);
-    const date = new Date().toISOString().split('T')[0];
-    
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+            const amountInCents = Math.round(amount * 100);
+            const date = new Date().toISOString().split('T')[0];
+            
+            await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch(error) {
+        console.error(error)
+        return {
+            message: "Database Error: Failed to Create Invoice"
+        }
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData){
-    const { customerId, amount, status } = UpdateInvoice.parse({
+    try {
+        const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get("cutomerId"),
         amount: formData.get("amount"),
         status: formData.get("status"),
-    })
+        })
 
-    const amountInCents = amount * 100;
+        const amountInCents = amount * 100;
 
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+    } catch(error){
+        console.error(error)
+
+        return {
+            message: "Database Error: Failed to update invoice"
+        }
+    }
 
   revalidatePath('/dashboard/invoices');
   redirect('dashboad/invoices');
