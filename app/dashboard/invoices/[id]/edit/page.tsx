d1c67867487f891d0144c1f7cc8b205f87b321f2
mrbryant21 <mrbryant250621@gmail.com>
2026-05-15 22:48:15 +0000

    mutating data functions and features implemented

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,7 +8,7 @@ export default async function Page(props: {params: Promise<{ id: string }>}) {
   const [ invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers()
-  ])
+  ]);
   return (
     <main>
       <Breadcrumbs
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from "@/app/lib/actions";
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,8 +19,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form action={createInvoice}>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -120,5 +123,6 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
+    
   );
 }
